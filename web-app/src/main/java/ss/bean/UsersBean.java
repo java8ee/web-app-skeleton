b426101d0b36fe31fd5c21f48fefca6902c2a35f
Sergey Meshchankin <java8ee@gmail.com>
2014-10-01 17:51:26 +0300

    Add sorting for ID and Login columns

--- web-app/src/main/java/ss/bean/UsersBean.java
+++ web-app/src/main/java/ss/bean/UsersBean.java
@@ -7,6 +7,9 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
 import java.io.Serializable;
+import java.lang.reflect.Field;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 @ManagedBean(name = "users")
@@ -15,8 +18,34 @@ public class UsersBean implements Serializable {
     @ManagedProperty("#{userService}")
     private UserService userService;
 
+    private Field sortField;
+    private boolean ascending;
+
     public List<User> getList() {
-        return userService.list();
+        List<User> list = userService.list();
+
+        if (this.sortField != null) {
+            Collections.sort(list,  new Comparator<User>() {
+                @Override
+                public int compare(User first, User last) {
+                    try {
+                        Object valueFirst = sortField.get(first);
+                        Object valueLast = sortField.get(last);
+                        if (valueFirst instanceof Comparable) {
+                            return ascending ?
+                                    ((Comparable) valueFirst).compareTo((Comparable) valueLast) :
+                                    ((Comparable) valueLast).compareTo((Comparable) valueFirst);
+                        } else {
+                            return 0;
+                        }
+                    } catch (IllegalAccessException cause) {
+                        return 0;
+                    }
+                }
+            });
+        }
+
+        return list;
     }
 
     /* SERVICES */
@@ -26,5 +55,26 @@ public class UsersBean implements Serializable {
 
     public void remove(User user) {
         userService.delete(user);
+    }
+
+    public String sort(String field) throws NoSuchFieldException {
+        Field sortField = getField(field);
+        if (sortField.equals(this.sortField)) {
+            this.ascending = !this.ascending;
+        } else {
+            this.sortField = sortField;
+            this.ascending = true;
+            this.sortField.setAccessible(true);
+        }
+
+        return null;
+    }
+
+    private Field getField(String field) throws NoSuchFieldException {
+        try {
+            return User.class.getDeclaredField(field);
+        } catch (NoSuchFieldException cause) {
+            return User.class.getField(field);
+        }
     }
 }
